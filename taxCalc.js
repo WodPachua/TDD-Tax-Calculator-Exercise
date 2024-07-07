a46a312797df816677fcbc03b3179044b3f4c826
wodpachua <molarag21996@gmail.com>
2024-07-07 14:45:33 +0300

    First test cycle

--- taxCalc.js
+++ taxCalc.js
@@ -1,2 +1,12 @@
 // PAIR PARTNER
 // Sarah Agemo
+
+export function taxCalc(earnings) {
+  let tax;
+
+  if (earnings < 12000) {
+    tax = 0;
+  }
+
+  return tax;
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 import { assert } from "chai";
 import { taxCalc } from "../taxCalc.js";
 
+describe("Tax calculator test", () => {
+  it("Earnings below 12,000 should have 0 tax", () => {
+    assert.equal(taxCalc(500), 0);
+    assert.equal(taxCalc(11999), 0);
+  });
+});
+
 // PAIR PARTNER
 // Sarah Agemo
